1a5dccc4bb4ff61ff2f43eefb3de596ce45c5fa8
enchf <e.ernesto.espinosa@gmail.com>
2017-12-09 04:58:54 -0600

    Ugly fold

--- src/higherorderfuns/2_Unfold.kt
+++ src/higherorderfuns/2_Unfold.kt
@@ -1,8 +1,12 @@
 package higherorderfuns
 
-fun Int.unfold(initial: List<Int> = listOf(), until: Int.() -> Boolean, block: Int.() -> Int): List<Int> =
-        with(initial.plus(this)) { if (until()) this else block().unfold(this, until, block) }
+fun Int.unfold(until: Int.() -> Int = { 1 },
+               block: Int.() -> Int = { dec() }): List<Int> =
+        listOf<Int>().let {
+            fun f(l: List<Int>, a: Int): List<Int> =
+                if (until() == a) l.plus(a)
+                else f(l.plus(a), a.block())
+            f(it, this)
+        }
 
-fun main(args: Array<String>) = 22
-        .unfold( until = { equals(1) }, block = { if (rem(2) == 0) div(2) else times(3).plus(1) } )
-        .let(::print)
+fun main(args: Array<String>) = 22.unfold { 11 }.let(::print)
